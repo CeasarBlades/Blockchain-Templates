049cf94fd5a5538ea2e8b4b8c512a19dc29cc9f1
ceasarblades <saula.atama@gmail.com>
2022-07-18 17:46:46 +1200

    rentals content added

--- Airbnb-starter/src/pages/Rentals.js
+++ Airbnb-starter/src/pages/Rentals.js
@@ -3,10 +3,25 @@ import "./Rentals.css";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import logo from "../images/airbnbRed.png";
-import { ConnectButton } from "web3uikit";
+import { ConnectButton, Icon} from "web3uikit";
 
 const Rentals = () => {
   const { state: searchFilters } = useLocation();
+  const rentalsList = [
+    {
+      attributes: {
+        city: "New York",
+        unoDescription: "3 Guests • 2 Beds • 2 Rooms",
+        dosDescription: "Wifi • Kitchen • Living Area",
+        imgUrl:
+          "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/3",
+        lat: "40.716862",
+        long: "-73.999005",
+        name: "Apartment in China Town",
+        pricePerDay: "3",
+      },
+    },
+  ];
 
   return (
     <>
@@ -16,12 +31,56 @@ const Rentals = () => {
           <img className="logo" src={logo} alt="logo"></img>
           </Link>
         </div>
-        <div className="searchReminder"></div>
+        <div className="searchReminder">
+        <div className="filter">{searchFilters.destination}</div>
+          <div className="vl" />
+          <div className="filter">
+          {`
+           ${searchFilters.checkIn.toLocaleString("default", {
+             month: "short",
+           })} 
+           ${searchFilters.checkIn.toLocaleString("default", {
+             day: "2-digit",
+           })} 
+           - 
+           ${searchFilters.checkOut.toLocaleString("default", {
+             month: "short",
+           })} 
+           ${searchFilters.checkOut.toLocaleString("default", {
+             day: "2-digit",
+           })}
+          `}
+          </div>
+          <div className="vl" />
+          <div className="filter">
+            {searchFilters.guests} Guests
+          </div>
+          <div className="searchFiltersIcon">
+            <Icon fill="#ffffff" size={20} svg="search" />
+          </div>
+
+        </div>
         <div className="lrContainers">
         <ConnectButton />
       </div>
     </div>
-    
+
+    <hr className="line" />
+      <div className="rentalsContent">
+        <div className="rentalsContentL">
+          Stays Available for your destination
+          { rentalsList &&
+            rentalsList.map(e => {
+              return(
+                
+              )
+            })
+          }
+        </div>
+        <div className="rentalsContentR">
+        </div>
+      </div>
+
     </>
   );
 };
